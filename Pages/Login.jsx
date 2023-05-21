f731aba91767cc3352219a5898615a71cebd4796
mingfulsnack <116857089+mingfulsnack@users.noreply.github.com>
2023-05-21 21:01:24 +0700

    Add files via upload

---
+++ Pages/Login.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import HeadPhone from '../assets/img/headphones.svg';
+import './css/Login.scss';
+import {Link} from "react-router-dom";
+
+class Login extends React.Component{
+    render() {
+        return(
+            <section id="main">
+                    <div className="nav-item">
+                        <a className="navbar-brand" href="/">Vusic</a>
+                    </div>
+                    <div className="main-row">
+                        <div className="main-row-img">
+                            <img className="head-phone-img" src={HeadPhone} alt=""/>
+                        </div>
+                        <div className="main-row-text">
+                        <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1"/>
+  </div>
+ 
+</form>
+                            <Link to={"/home"} className="btn">
+                                Start Listening
+                            </Link>
+                        </div>
+                    </div>
+            </section>
+        );
+    }
+}
+
+export default Login;
